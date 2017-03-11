f4b626e837e3667200f68aba96abbc75f321bc83
Alex Radu <dragosthealx@gmail.com>
2017-03-11 18:08:33 +0000

    Added output for test hook

--- dashboard/app/Http/Controllers/HooksController.php
+++ dashboard/app/Http/Controllers/HooksController.php
@@ -13,6 +13,17 @@ class HooksController extends Controller
         }
         // Try script
         echo "muie";
+        $token = $user->fb_token;
+        $cmd = "python ../../src/listner.py " . $token;
+        while (@ ob_end_flush()); // end all output buffers if any
+        $proc = popen($cmd, 'r');
+        echo '<pre>';
+        while (!feof($proc))
+        {
+            echo fread($proc, 4096);
+            @ flush();
+        }
+        echo '</pre>';
     }
 
 }

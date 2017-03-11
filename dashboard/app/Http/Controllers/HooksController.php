17691faaa924c8815921f27255de2ab1bc61e085
Alex Radu <dragosthealx@gmail.com>
2017-03-11 21:34:50 +0000

    added migration for sentiment analyis. fixed video download

--- dashboard/app/Http/Controllers/HooksController.php
+++ dashboard/app/Http/Controllers/HooksController.php
@@ -82,7 +82,7 @@ class HooksController extends Controller
             mkdir('../../videos/' . $user->id);
         }
         // Download video there
-        file_put_contents('../../videos/' . $user->id . '/' . $video->fb_id + '.mp4', $source);
+        file_put_contents('../../videos/' . $user->id . '/' . $video->fb_id + '.mp4', file_get_contents($source));
 
         // Go to videos
         return redirect()->to('/videos/' . $video->id);

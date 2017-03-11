47926098934848c1ac88570d268dfbc8a2f82475
Alex Radu <dragosthealx@gmail.com>
2017-03-11 16:05:27 +0000

    Added fb tokens

--- dashboard/app/Http/Controllers/Auth/OAuthController.php
+++ dashboard/app/Http/Controllers/Auth/OAuthController.php
@@ -30,17 +30,30 @@ class OAuthController extends Controller {
         $fb_user = Socialize::with($provider)
                     ->fields(['first_name', 'last_name', 'email'])->user();
         $token = $fb_user->token;
+        // Get long lived token
+        $ch = curl_init();
+        $url = "https://graph.facebook.com/v2.8/oauth/access_token?grant_type=fb_exchange_token&client_id=" . env('FB_APP_ID') . "&client_secret=" . env('FB_APP_SECRET') . "&fb_exchange_token=" . $token . "&redirect_uri=" . urlencode(env('APP_URL'));
+        echo $url;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result = json_decode(curl_exec($ch), 1);
+        curl_close($ch);
+        if(!isset($result["access_token"])) {
+            echo "There was an error.";
+        }
+        $token = $result["access_token"];
+        // Try to login
         $fb_user = $fb_user->user;
         $user = User::where('fb_id', $fb_user["id"])->first();
         if(!$user) {
             $user = new User();
             $user->name = $fb_user["first_name"] . ' ' . $fb_user["last_name"];
             $user->email = $fb_user["email"];
-            $user->fb_token = $token;
             $user->fb_id = $fb_user["id"];
             $user->password = bcrypt(str_random(40));
-            $user->save();
         }
+
+        $user->save();
         Auth::login($user);
         return redirect('/home');
     }

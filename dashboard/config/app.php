16ef56c623ddee466ec1c33b4bd0c7242dd69a77
Alex Radu <dragosthealx@gmail.com>
2017-03-12 09:46:24 +0000

    Asd

--- dashboard/config/app.php
+++ dashboard/config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'fb-hack-2017',
+    'name' => 'Live Insights',
 
     /*
     |--------------------------------------------------------------------------
